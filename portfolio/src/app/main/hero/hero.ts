af597853ae78ce94b231fad8136dffb579b56fea
Manuel <manuzcore1@gmail.com>
2025-09-28 20:03:40 +0200

    feat: added hero section and styling

--- portfolio/src/app/main/hero/hero.ts
+++ portfolio/src/app/main/hero/hero.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
 export class Hero {
-
+  protected readonly profileImage = signal<string>('assets/img/profile.jpg');
+  protected readonly developerName = signal<string>('Manuel Giehl');
+  protected readonly developerTitle = signal<string>('FULLSTACK DEVELOPER');
+  protected readonly email = signal<string>('manuelgiehl@gmail.com');
+  
+  onCtaClick(): void {
+    console.log('Let\'s talk button clicked!');
+  }
 }
